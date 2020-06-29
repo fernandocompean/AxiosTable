042d62824cd8f86db375f360f10b0f70920bf780
Fernando Treviño Compean <fernandocompean@Fernandos-MacBook-Pro.local>
2020-06-28 21:24:52 -0500

    [ADD]: Request User Info & Showing it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
 
-function App() {
+const App = () => {
+  const url = 'https://jsonplaceholder.typicode.com/users'
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    axios.get(url).then(json => setData(json.data))
+  }, [])
+
+  const dataTable = () => {
+    return data.map(user => {
+      return (
+        <tr>
+          <td>{user.name}</td>
+          <td>{user.email}</td>
+          <td>{user.phone}</td>
+          <td>{user.company.name}</td>
+        </tr>
+      )
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Axios Table</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Email</th>
+            <th>Phone</th>
+            <th>Company</th>
+            </tr> 
+        </thead>
+        <tbody>{dataTable()}</tbody>
+      </table>
     </div>
-  );
+  )
 }
+
+ReactDOM.render(<App />, document.getElementById('root'))
 
 export default App;
